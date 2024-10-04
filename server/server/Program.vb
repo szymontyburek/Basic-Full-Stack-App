b62ce7e9d8194623c3fe05dc1e7c1f2ef95bce35
Szymon Tyburek <szymontyburek10@gmail.com>
2024-10-04 15:03:18 -0700

    removed unnecessary code from vb file

--- server/server/Program.vb
+++ server/server/Program.vb
@@ -3,10 +3,8 @@ Imports Microsoft.Extensions.DependencyInjection
 
 Module Program
     Sub Main(args As String())
-        ' Create a builder for the application
         Dim builder = WebApplication.CreateBuilder(args)
 
-        ' Add CORS services with a specific policy
         builder.Services.AddCors(Function(options)
                                      options.AddPolicy("AllowAllOrigins", Sub(policy)
                                                                               policy.AllowAnyOrigin() _
@@ -15,24 +13,14 @@ Module Program
                                                                           End Sub)
                                  End Function)
 
-        ' Build the application
         Dim app = builder.Build()
 
-        ' Use the CORS policy
         app.UseCors("AllowAllOrigins")
 
-        ' Define a simple GET endpoint at the root
         app.MapGet("/", Function()
                             Dim x = 5 + 2
                             Return $"The result is: {x}"
                         End Function)
-
-        ' Define an example endpoint for testing CORS
-        app.MapGet("/api/test", Function()
-                                    Return "CORS is working!"
-                                End Function)
-
-        ' Run the application
         app.Run()
     End Sub
 End Module
