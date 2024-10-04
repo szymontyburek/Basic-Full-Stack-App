bad5a6d7b16a48d68a9d51201d4ab1394f238764
Szymon Tyburek <szymontyburek10@gmail.com>
2024-10-04 15:01:21 -0700

    hitting GET api endpoint in vb backend and return response packet to client

--- Basic VB Razor Web App/wwwroot/js/site.js
+++ Basic VB Razor Web App/wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿const getRequestBtn = document.getElementById("getRequest");
+const serverPort = "http://localhost:5000";
+
 getRequestBtn.addEventListener("click", function () {
-    alert("hi");
+    fetch(serverPort)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            debugger;
+        })
+        .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
 })
--- server/server/Program.vb
+++ server/server/Program.vb
@@ -1,7 +1,38 @@
-Imports System
+Imports Microsoft.AspNetCore.Builder
+Imports Microsoft.Extensions.DependencyInjection
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        ' Create a builder for the application
+        Dim builder = WebApplication.CreateBuilder(args)
+
+        ' Add CORS services with a specific policy
+        builder.Services.AddCors(Function(options)
+                                     options.AddPolicy("AllowAllOrigins", Sub(policy)
+                                                                              policy.AllowAnyOrigin() _
+                                                                          .AllowAnyMethod() _
+                                                                          .AllowAnyHeader()
+                                                                          End Sub)
+                                 End Function)
+
+        ' Build the application
+        Dim app = builder.Build()
+
+        ' Use the CORS policy
+        app.UseCors("AllowAllOrigins")
+
+        ' Define a simple GET endpoint at the root
+        app.MapGet("/", Function()
+                            Dim x = 5 + 2
+                            Return $"The result is: {x}"
+                        End Function)
+
+        ' Define an example endpoint for testing CORS
+        app.MapGet("/api/test", Function()
+                                    Return "CORS is working!"
+                                End Function)
+
+        ' Run the application
+        app.Run()
     End Sub
 End Module
