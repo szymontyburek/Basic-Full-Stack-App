34b9edfeff9d3f3f2a271051106f76caabadac88
Szymon Tyburek <szymontyburek10@gmail.com>
2024-10-05 14:38:29 -0700

    gave the GET API endpoint a specified endpoint within the corresponding port

--- server/server/Program.vb
+++ server/server/Program.vb
@@ -20,19 +20,19 @@ Module Program
 
         app.UseCors("AllowAllOrigins")
 
-        app.MapGet("/", Async Function()
-                            Dim SQL As New SQLControl()
-                            Dim res = CType(Await SQL.GETquery("SELECT e.first_name + ' ' + e.last_name AS Name, d.title AS Department, d.description AS Responsibilities FROM Employees e INNER JOIN Departments d ON e.departmentId=d.id"), IDictionary(Of String, Object))
-                            SQL.connection.Close() 'data is NOT visible if connection to DB has been closed. That is why it is closed AFTER receiving data from SQLControl class
+        app.MapGet("/getEmpInfo", Async Function()
+                                      Dim SQL As New SQLControl()
+                                      Dim res = CType(Await SQL.GETquery("SELECT e.first_name + ' ' + e.last_name AS Name, d.title AS Department, d.description AS Responsibilities FROM Employees e INNER JOIN Departments d ON e.departmentId=d.id"), IDictionary(Of String, Object))
+                                      SQL.connection.Close() 'data is NOT visible if connection to DB has been closed. That is why it is closed AFTER receiving data from SQLControl class
 
-                            If res("success") Then
-                                Console.WriteLine("Successful query")
-                            Else
-                                Console.WriteLine("Unsuccessful query")
-                            End If
+                                      If res("success") Then
+                                          Console.WriteLine("Successful query")
+                                      Else
+                                          Console.WriteLine("Unsuccessful query")
+                                      End If
 
-                            Return res
-                        End Function)
+                                      Return res
+                                  End Function)
         app.Run()
     End Sub
 End Module
