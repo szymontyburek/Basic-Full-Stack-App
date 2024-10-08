c91ca84ddc2a511b34ea738b701508ea9a41114c
Szymon Tyburek <szymontyburek10@gmail.com>
2024-10-08 09:27:03 -0700

    sending out GET request to server on initial load of webpage. Created a table and filled it with data from server response dynamically

--- Basic VB Razor Web App/wwwroot/js/site.js
+++ Basic VB Razor Web App/wwwroot/js/site.js
@@ -1,16 +1,43 @@
 ﻿const getRequestBtn = document.getElementById("getRequest");
+const readOnlyTable = document.getElementById("readOnlyTable");
 const serverPort = "http://localhost:5000/";
 
+fetch(serverPort + "getEmpInfo")
+    .then((response) => {
+        return response.json();
+    })
+    .then((res) => {
+        console.log(res);
+
+        const data = res.data;
+        let columns = [];
+
+        for (let i = 0; i < data.length; i++) {
+            const rowObj = data[i];
+            const headers = Object.keys(rowObj);
+
+            const tr = document.createElement("tr");
+            readOnlyTable.appendChild(tr);
+
+            for (const header of headers) {
+                let HTMLelem;
+
+                if (i === 0) {
+                    HTMLelem = document.createElement("th");
+                    HTMLelem.innerHTML = header;
+                } else {
+                    HTMLelem = document.createElement("td");
+                    HTMLelem.innerHTML = rowObj[header];
+                }
+
+                tr.appendChild(HTMLelem);
+            }
+        }
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
 getRequestBtn.addEventListener("click", function () {
-    fetch(serverPort + "getEmpInfo")
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
 
 })
